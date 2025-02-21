9238bf39e7c20e3ee72f53b8e24f97995ad630d2
srikanth <rmsrikanth25.gmail.com>
2025-02-21 14:17:14 +0530

    let 8 props example finished

--- src/App.js
+++ src/App.js
@@ -2,10 +2,56 @@
 import ExpenseItem from "./components/ExpenseItem.js";
 
 function App() {
+  const expenses = [
+    {
+      id: 0,
+      title: "Car Insurence",
+      amount: 200,
+      date: new Date(2022, 3, 22),
+    },
+    {
+      id: 1,
+      title: "school fees",
+      amount: 230,
+      date: new Date(2022, 3, 23),
+    },
+    {
+      id: 2,
+      title: "room rent",
+      amount: 500,
+      date: new Date(2022, 3, 24),
+    },
+    {
+      id: 3,
+      title: "tution fees",
+      amount: 200,
+      date: new Date(2022, 3, 25),
+    },
+  ];
+
   return (
     <div className="app-container">
-      {/* create component ExponentItem and add that here. */}
-      <ExpenseItem />
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+      />
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+      />
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+      />
+      <ExpenseItem
+        date={expenses[3].date}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+      />
+      1
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,24 @@
 // import the css
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  let year = props.date.getFullYear();
+  let month = props.date.toLocaleDateString("en-US", { month: "long" });
+  let day = props.date.toLocaleDateString("en-US", { day: "2-digit" });
+
   return (
+    // here it is will add to all the items by default
+    // to make unique values to all the components use variable
+    // creating and using here is not correct way as it is used in app.js we create there and add them
     <div className="expense-item">
-      <div>22 march 2020</div>
+      <div>
+        <div>{year}</div>
+        <div>{month}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <div>expense</div>
-        <div className="expense-item__price">$200</div>
+        <div>{props.title}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
