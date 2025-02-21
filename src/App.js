4ecd279cede5688bee86a71b1f121846b0f1d95c
srikanth <rmsrikanth25.gmail.com>
2025-02-21 12:21:19 +0530

    last initializing the project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import the css
+import "./App.css";
+
+// write the boiler plate
+
+// function with the same name
+function App() {
+  return (
+    <div className="app-container">
+        
+    </div>
+  );
+}
+
+// do the export default
+export default App;
+
+// Parsing error: Adjacent JSX elements must be wrapped in an enclosing tag
+// pass it in a wrapper like a div
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// import the index.css for styling of the page.
+import "./index.css";
+
+// import the App.js
+import App from "./App.js";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<App />);
+
+// index.js is the js code which is going to add to the root in the index.html page, which is the only page in our application
