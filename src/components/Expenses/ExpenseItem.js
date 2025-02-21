ee34cdbfa95fb0b45b0a667ad49d6297232aecc8
srikanth <rmsrikanth25.gmail.com>
2025-02-21 16:25:23 +0530

    lect 11 - state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,25 @@
 // write the boiler plate
 // import the css
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate.js";
 import Card from "../UI/Card.js";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("New Title");
+    // alert("clicked!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <div>{props.title}</div>
+        <div>{title}</div>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change title</button>
       </div>
     </Card>
   );
